04fed801026b21de6315d3cef5bc776a52ed956b
Samina Fu <sufuf3@gmail.com>
2018-11-22 18:19:05 +0000

    Add notebooks & run_jupyter.sh
    Ref: https://github.com/tensorflow/tensorflow/tree/master/tensorflow/tools/docker

--- production/tensorrt_18.10-py3/run_jupyter.sh
+++ production/tensorrt_18.10-py3/run_jupyter.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,20 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import os
-from IPython.lib import passwd
 
-c = c  # pylint:disable=undefined-variable
-c.NotebookApp.ip = '0.0.0.0'  # https://github.com/jupyter/notebook/issues/3946
-c.NotebookApp.port = int(os.getenv('PORT', 8888))
-c.NotebookApp.open_browser = False
 
-# sets a password if PASSWORD is set in the environment
-if 'PASSWORD' in os.environ:
-  password = os.environ['PASSWORD']
-  if password:
-    c.NotebookApp.password = passwd(password)
-  else:
-    c.NotebookApp.password = ''
-    c.NotebookApp.token = ''
-  del os.environ['PASSWORD']
+jupyter notebook "$@"
