b23d9419ee27c6ca2cc214b41b685a8b977b052b
Yi Fu Ciou <stevennickj@users.noreply.github.com>
2018-12-07 01:40:03 -0800

    Change working directory from /notebooks to /workspace

--- production/tensorflow_18.08-py3/init.sh
+++ production/tensorflow_18.08-py3/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 tmpHome=$HOME
-export $(sudo cat /proc/1/environ | tr '\0' '\n')
+export $(sudo cat /proc/1/environ | tr '\0' '\n' | sed 's/CC_OPT_FLAGS.*//')
+export CC_OPT_FLAGS="-march=sandybridge -mtune=broadwell"
 export HOME=$tmpHome
 export BAZELRC=$HOME/.bazelrc
